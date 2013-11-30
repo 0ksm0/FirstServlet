a362bc2765853e1dddafa4c278a210cbc9170cfb
user <ghghg@ukr.net>
2013-11-30 17:02:44 +0200

    first Servlet

---
+++ src/main/resources/Servlet1.java
@@ -0,0 +1,22 @@
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: user
+ * Date: 30.11.13
+ * Time: 16:32
+ * To change this template use File | Settings | File Templates.
+ */
+public class Servlet1 extends HttpServlet {
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+    }
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+    }
+}
